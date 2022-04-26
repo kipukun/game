9cbca9eb89285d1d7491fdb44765fa4ae6bdefc8
kipukun <kipukun@waifu.club>
2022-04-26 03:34:03 -0400

    engine(audio): errors

--- engine/audio.go
+++ engine/audio.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2/audio"
 	"github.com/hajimehoshi/ebiten/v2/audio/mp3"
+	"github.com/kipukun/game/engine/errors"
 )
 
 type audioHandler struct {
@@ -48,22 +49,24 @@ func (ap *audioHandler) SetVolume(v float64) {
 
 // Player is a helper method to give an audio.Player from an MP3 asset.
 func (ah *audioHandler) Player(path string) (*AudioPlayer, error) {
+	var op errors.Op = "Player"
+
 	fd, err := asset(ah.files, path)
 	if err != nil {
-		return nil, err
+		return nil, errors.Error(op, "error getting asset", err)
 	}
 	bs, err := io.ReadAll(fd)
 	if err != nil {
-		return nil, err
+		return nil, errors.Error(op, "error reading all", err)
 	}
 	mp3, err := mp3.DecodeWithSampleRate(ah.sr, bytes.NewReader(bs))
 	if err != nil {
-		return nil, err
+		return nil, errors.Error(op, "error decoding mp3", err)
 	}
 
 	p, err := ah.actx.NewPlayer(mp3)
 	if err != nil {
-		return nil, err
+		return nil, errors.Error(op, "error creating Player from context", err)
 	}
 
 	ap := &AudioPlayer{p: p}
